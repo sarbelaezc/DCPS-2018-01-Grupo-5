48c02ac9b2cef96d78e2bb89ee6c341d8de75708
sarbelaezc <sarbelaezc@unal.edu.co>
2018-04-04 11:11:58 -0500

    "configuración" con firebase

--- LinkedUN/app/adapters/application.js
+++ LinkedUN/app/adapters/application.js
@@ -1,4 +1,14 @@
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
+    var ENV = {
+      firebase: {
+        apiKey: "AIzaSyAGKn9m6Lm7lM8LrAqmT0EGKAK1z6n001Q",
+        authDomain: "dcps-ejemplo-ember.firebaseapp.com",
+        databaseURL: "https://dcps-ejemplo-ember.firebaseio.com",
+        projectId: "dcps-ejemplo-ember",
+        storageBucket: "dcps-ejemplo-ember.appspot.com",
+        messagingSenderId: "1082607898569"
+      },
+    }
 });
