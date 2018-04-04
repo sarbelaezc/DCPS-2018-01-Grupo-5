ca1e0e80ee87d1092b4621f4113dc24c040053e3
caolivap <36514231+caolivap@users.noreply.github.com>
2018-04-04 11:24:58 -0500

    Ember firebase

--- LinkedUN/app/adapters/application.js
+++ LinkedUN/app/adapters/application.js
@@ -3,12 +3,12 @@ import FirebaseAdapter from 'emberfire/adapters/firebase';
 export default FirebaseAdapter.extend({
     var ENV = {
       firebase: {
-        apiKey: "AIzaSyAGKn9m6Lm7lM8LrAqmT0EGKAK1z6n001Q",
-        authDomain: "dcps-ejemplo-ember.firebaseapp.com",
-        databaseURL: "https://dcps-ejemplo-ember.firebaseio.com",
-        projectId: "dcps-ejemplo-ember",
-        storageBucket: "dcps-ejemplo-ember.appspot.com",
-        messagingSenderId: "1082607898569"
+            apiKey: "AIzaSyBNnevTmYvCZ0Pf1xpXiNAioNVfBcmuUWY",
+            authDomain: "linked-un.firebaseapp.com",
+            databaseURL: "https://linked-un.firebaseio.com",
+            projectId: "linked-un",
+            storageBucket: "linked-un.appspot.com",
+            messagingSenderId: "444181491968"
       },
     }
 });
