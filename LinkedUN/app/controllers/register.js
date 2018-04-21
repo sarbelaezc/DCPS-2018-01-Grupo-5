e70d375943de3ca266a3852032ad92145566ae11
sarbelaezc <sarbelaezc@unal.edu.co>
2018-04-21 08:13:54 -0500

    Guardado de datos estudiante en firebase

--- LinkedUN/app/controllers/register.js
+++ LinkedUN/app/controllers/register.js
@@ -6,5 +6,17 @@ export default Controller.extend({
       this.set('vinculacion',vinculacion);
       console.log(this.get('vinculacion'));
     },
+    register(name, email, idNumber, vinculacion, dateOfBirth, password, confirmPassword){
+      if(vinculacion == 'estudiante'){
+        var newEstudiante = this.store.createRecord('estudiante',{
+          name: this.name,
+          email: this.email + '@unal.edu.co',
+          idNumber: this.idNumber,
+          dateOfBirth: new Date(this.dateOfBirth),
+          password: this.password
+        });
+        newEstudiante.save();
+      }
+    },
   }
 });
