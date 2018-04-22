86e9b641592adf45f011e3b70204ea2d4c117e84
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-04-22 17:29:56 -0500

    pop-ups para el login

--- LinkedUN/app/controllers/login.js
+++ LinkedUN/app/controllers/login.js
@@ -1,4 +1,88 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  actions: {
+    login(email, password){
+
+    },
+  }
 });
+
+// -----------------------------------------------------------------------------
+//PopUp
+function CustomAlert(txt) {
+  var ALERT_TITLE = "Inicio de sesión exitoso!";
+  var ALERT_BUTTON_TEXT = "Ok";
+  var mObj,alertObj,h1,msg,btn;
+
+  if(document.getElementById("modalContainer")) return;
+
+  mObj = document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+  mObj.id = "modalContainer";
+  mObj.style.height = document.documentElement.scrollHeight + "px";
+
+  alertObj = mObj.appendChild(document.createElement("div"));
+  alertObj.id = "alertBox";
+  if(document.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+  alertObj.style.left = (document.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
+  alertObj.style.visiblity="visible";
+
+  h1 = alertObj.appendChild(document.createElement("h2"));
+  h1.appendChild(document.createTextNode(ALERT_TITLE));
+
+  msg = alertObj.appendChild(document.createElement("p"));
+  //msg.appendChild(document.createTextNode(txt));
+  msg.innerHTML = txt;
+
+  btn = alertObj.appendChild(document.createElement("a"));
+  btn.id = "Btn";
+  btn.appendChild(document.createTextNode(ALERT_BUTTON_TEXT));
+  btn.href = "#";
+  btn.focus();
+  btn.onclick = function() {
+    removeCustomAlert();
+    return false;
+  }
+  alertObj.style.display = "block";
+}
+
+//PopUp Error
+function errorCustomAlert(txt) {
+  var ALERT_TITLE = "Error!";
+  var ALERT_BUTTON_TEXT = "Ok";
+  var mObj,alertObj,h1,msg,btn;
+
+  if(document.getElementById("modalContainer")) return;
+
+  mObj = document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+  mObj.id = "modalContainer";
+  mObj.style.height = document.documentElement.scrollHeight + "px";
+
+  alertObj = mObj.appendChild(document.createElement("div"));
+  alertObj.id = "alertBox";
+  if(document.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+  alertObj.style.left = (document.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
+  alertObj.style.visiblity="visible";
+
+  h1 = alertObj.appendChild(document.createElement("h1"));
+  h1.appendChild(document.createTextNode(ALERT_TITLE));
+
+  msg = alertObj.appendChild(document.createElement("p"));
+  //msg.appendChild(document.createTextNode(txt));
+  msg.innerHTML = txt;
+
+  btn = alertObj.appendChild(document.createElement("a"));
+  btn.id = "errorBtn";
+  btn.appendChild(document.createTextNode(ALERT_BUTTON_TEXT));
+  btn.href = "#";
+  btn.focus();
+  btn.onclick = function() {
+    removeCustomAlert();
+    return false;
+  }
+  alertObj.style.display = "block";
+}
+
+function removeCustomAlert() {
+  document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));
+}
