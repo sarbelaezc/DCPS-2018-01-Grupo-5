bd0ba71830ff0662d7a0725a4438554473d2360b
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-04-22 23:14:56 -0500

    tests

---
+++ LinkedUN/tests/unit/controllers/publica-convocatoria-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | publica-convocatoria', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:publica-convocatoria');
+    assert.ok(controller);
+  });
+});
