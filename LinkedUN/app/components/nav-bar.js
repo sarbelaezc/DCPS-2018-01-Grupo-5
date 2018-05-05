e3954fe0a9c4c11fea6be8dc8622c4d0b2d16093
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-05-04 21:33:53 -0500

    componente barra de navegacion

---
+++ LinkedUN/app/components/nav-bar.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  actions:{
+    responsive() {
+      var x = document.getElementById("myTopnav");
+      if (x.className === "topnav") {
+          x.className += " responsive";
+      } else {
+          x.className = "topnav";
+      }
+    }
+  },
+});
