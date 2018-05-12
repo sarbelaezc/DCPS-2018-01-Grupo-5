553a5eaa06134f5b8e836f57634b518af7b4f43a
sarbelaezc <sarbelaezc@unal.edu.co>
2018-05-12 10:47:46 -0500

    Se determina el usuario actvo pero no se puede acceder a sus atributos

--- LinkedUN/app/controllers/convocatories.js
+++ LinkedUN/app/controllers/convocatories.js
@@ -10,12 +10,19 @@ export default Controller.extend({
     return this.get('model.convocatories');
   }),
 
+  activeUser: computed('model.students.@each', function() {
+    return this.get('model.students').filterBy('uId',this.get('session.currentUser.uid'));
+  }),
+
   actions: {
     apply(convocatory){
       const student = this.store.query('student',{
         orderBy: 'uId',
         equalTo: this.get('session.currentUser.uid')
       });
+      console.log(this.get('session.currentUser.uid'));
+      console.log(this.activeUser);
+      console.log(this.activeUser._internalModel.__data.uId);
       student.get('convocatories').pushObject(convocatory);
       // student.set('convocatories', convocatory);
       student.save();
