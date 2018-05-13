fdfcbe06c68146c00ce39ba84649d22c0c0945a8
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-05-13 17:49:10 -0500

    Modelos con atributos para todos los usuarios

--- LinkedUN/app/models/professor.js
+++ LinkedUN/app/models/professor.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  uId: DS.attr('string'),
+  name: DS.attr('string'),
+  email: DS.attr('string'),
+  idNumber: DS.attr('number'),
+  dateOfBirth: DS.attr('date'),
+  password: DS.attr('string'),
+  phone: DS.attr('number',{defaultValue:0}),
+  faculty: DS.attr('string',{defaultValue:'null'}),
 });
