bd5575b67c7a985002a80ea338e5e45c912de3e2
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-05-23 21:45:46 -0500

    revisa si hay puestos disponibles y muestra un mensaje al aplicar

--- LinkedUN/app/controllers/convocatories.js
+++ LinkedUN/app/controllers/convocatories.js
@@ -21,17 +21,108 @@ export default Controller.extend({
       var convocatory = this.get('model.convocatories').filterBy('id', convocatoryId);
 
       this.store.findRecord('student', student[0].id).then(function(student) {
-        student.get('convocatories').addObject(convocatory);
-        // student.set('convocatories', convocatory);
+        student.get('convocatories').forEach((existingConvocatory)=>{
+          convocatory.addObject(existingConvocatory);
+        });
+        student.set('convocatories', convocatory);
         student.save();
       });
 
       this.store.findRecord('convocatory', convocatory[0].id).then(function(convocatory) {
-        convocatory.get('students').addObject(student);
-        // convocatory.set('students', student);
-        convocatory.save();
+        var places = convocatory.get('places');
+        if (places > 0) {
+          convocatory.get('students').forEach((existingStudents)=>{
+            student.addObject(existingStudents);
+          });
+
+          convocatory.set('students', student);
+          convocatory.save();
+
+          CustomAlert('Se ha aplicado a la convocatoria');
+        }else {
+          errorCustomAlert('No hay puestos disponibles en esta convocatoria');
+        }
       });
     },
 
   },
 });
+
+// -----------------------------------------------------------------------------
+//PopUp
+function CustomAlert(txt) {
+  var ALERT_TITLE = "Registro satisfactorio!";
+  var ALERT_BUTTON_TEXT = "Ok";
+  var mObj,alertObj,h1,msg,btn;
+
+  if(document.getElementById("modalContainer")) return;
+
+  mObj = document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+  mObj.id = "modalContainer";
+  mObj.style.height = document.documentElement.scrollHeight + "px";
+
+  alertObj = mObj.appendChild(document.createElement("div"));
+  alertObj.id = "alertBox";
+  if(document.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+  alertObj.style.left = (document.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
+  alertObj.style.visiblity="visible";
+
+  h1 = alertObj.appendChild(document.createElement("h2"));
+  h1.appendChild(document.createTextNode(ALERT_TITLE));
+
+  msg = alertObj.appendChild(document.createElement("p"));
+  //msg.appendChild(document.createTextNode(txt));
+  msg.innerHTML = txt;
+
+  btn = alertObj.appendChild(document.createElement("a"));
+  btn.id = "Btn";
+  btn.appendChild(document.createTextNode(ALERT_BUTTON_TEXT));
+  btn.href = "#";
+  btn.focus();
+  btn.onclick = function() {
+    removeCustomAlert();
+    return false;
+  }
+  alertObj.style.display = "block";
+}
+
+//PopUp Error
+function errorCustomAlert(txt) {
+  var ALERT_TITLE = "Error!";
+  var ALERT_BUTTON_TEXT = "Ok";
+  var mObj,alertObj,h1,msg,btn;
+
+  if(document.getElementById("modalContainer")) return;
+
+  mObj = document.getElementsByTagName("body")[0].appendChild(document.createElement("div"));
+  mObj.id = "modalContainer";
+  mObj.style.height = document.documentElement.scrollHeight + "px";
+
+  alertObj = mObj.appendChild(document.createElement("div"));
+  alertObj.id = "alertBox";
+  if(document.all && !window.opera) alertObj.style.top = document.documentElement.scrollTop + "px";
+  alertObj.style.left = (document.documentElement.scrollWidth - alertObj.offsetWidth)/2 + "px";
+  alertObj.style.visiblity="visible";
+
+  h1 = alertObj.appendChild(document.createElement("h1"));
+  h1.appendChild(document.createTextNode(ALERT_TITLE));
+
+  msg = alertObj.appendChild(document.createElement("p"));
+  //msg.appendChild(document.createTextNode(txt));
+  msg.innerHTML = txt;
+
+  btn = alertObj.appendChild(document.createElement("a"));
+  btn.id = "errorBtn";
+  btn.appendChild(document.createTextNode(ALERT_BUTTON_TEXT));
+  btn.href = "#";
+  btn.focus();
+  btn.onclick = function() {
+    removeCustomAlert();
+    return false;
+  }
+  alertObj.style.display = "block";
+}
+
+function removeCustomAlert() {
+  document.getElementsByTagName("body")[0].removeChild(document.getElementById("modalContainer"));
+}
