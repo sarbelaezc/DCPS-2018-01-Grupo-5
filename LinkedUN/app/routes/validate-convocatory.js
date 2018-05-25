1094ba99ed6fa8bca61327de2bd60bb1f13ef084
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-05-24 20:55:16 -0500

    Validar convocatoria listo

--- LinkedUN/app/routes/validate-convocatory.js
+++ LinkedUN/app/routes/validate-convocatory.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  beforeModel: function() {
+    if (!this.get('session.isAuthenticated')) {
+      this.transitionTo('login');
+    }
+  },
+
+  model: function() {
+    return {
+      students: this.store.findAll('student'),
+      convocatories: this.store.findAll('convocatory'),
+    };
+  },
+
 });
---
+++ LinkedUN/tests/unit/controllers/validate-convocatory-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Controller | validate-convocatory', function(hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function(assert) {
+    let controller = this.owner.lookup('controller:validate-convocatory');
+    assert.ok(controller);
+  });
+});
