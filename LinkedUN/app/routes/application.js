9404ef2f4cbe04dd97b14aebcd41f7df1fd8b7f4
Santiago Arbeláez C <sarbelaezc@unal.edu.co>
2018-05-25 02:39:57 -0500

    Discernimiento de tipo de usuario en la barra de navegación

--- LinkedUN/app/routes/application.js
+++ LinkedUN/app/routes/application.js
@@ -4,9 +4,18 @@ import { inject as service } from '@ember/service';
 export default Route.extend({
   firebaseApp: service(),
   session: service(),
-  
+
   beforeModel: function() {
    return this.get("session").fetch().catch(function() {}); // <--- note that we now return the promise here
- },
+  },
+
+  model(){
+    var uid = this.get('session.currentUser.uid');
+    console.log(uid);
+    return this.store.query('user',{
+      orderBy: 'uId',
+      equalTo: uid,
+    });
+  },
 
 });
